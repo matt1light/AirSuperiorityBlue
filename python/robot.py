b3ca95068adc2482539c1708727b4c2ea662ad54
matt1light <matt1light@gmail.com>
2021-03-27 11:31:53 -0400

    Extract gamestate loop from robot

--- python/robot.py
+++ python/robot.py
@@ -8,6 +8,8 @@
 #
 # IMPORTANT: for each successful call to simxStart, there
 # should be a corresponding call to simxFinish at the end!
+import time
+import random
 
 try:
     import sim
@@ -22,36 +24,125 @@ except:
 
 import time
 
+class WatchDog:
+    def __init__(self, clientID):
+        # Fetch the object handle for the ball
+        self.res, self.ball = sim.simxGetObjectHandle(
+            clientID, "Ball", sim.simx_opmode_blocking
+        )
 
-class SoccerRobot:
+        # The location of the ball in the last game loop
+        self.last_ball_position = sim.simxGetObjectPosition(clientID, self.ball, -1, sim.simx_opmode_blocking)[1]
+
+        # The last time that the ball was moved, if this is more than the timeout value
+        self.last_moved_time = time.time()
+
+        # the number of seconds until the ball's location is reset
+        self.timeout = 10
+
+        self.clientID = clientID
+
+    def update_watchdog(self):
+        # Get the current position of the ball
+        new_position = sim.simxGetObjectPosition(self.clientID, self.ball, -1, sim.simx_opmode_blocking)[1]
+        # Get the current time
+        current_time = time.time()
+        # If the ball has moved then update the stored ball location, and set the last moved time to the current time
+        if (new_position != self.ball_position):
+            self.last_ball_position = new_position
+            self.last_moved_time = current_time
+        else:
+            # If the ball hasn't moved, check if the time since the last move is more than the timeout value
+            time_elapsed = current_time - self.last_moved_time
+            if (time_elapsed >= self.timeout):
+                # If the ball has not moved in timeout number of seconds, we reset the location of the ball
+                sim.simxSetObjectPosition(
+                    self.clientID, self.ball, -1, self.initial_ball_position, sim.simx_opmode_blocking
+                )
+
+class Scoreboard:
+    def __init__(self, clientID):
+        self.robotAScore = 0
+        self.robotBScore = 0
+        self.dialog = 0
+        self.clientID = clientID
+        self._updateScoreBoard()
+
+    def scoreA(self):
+        self.robotAScore += 1
+        self._updateScoreBoard()
+
+    def scoreB(self):
+        self.robotBScore += 1
+        self._updateScoreBoard()
+
+    def _updateScoreBoard(self):
+        if (self.dialog != 0):
+            sim.simxEndDialog(self.clientID,self.dialog,sim.simx_opmode_oneshot)
+        _, self.dialog, _ = sim.simxDisplayDialog(self.clientID, " ", f'A: {self.robotAScore} || B: {self.robotBScore}', 0, " ", None , None, sim.simx_opmode_blocking)
+
+class GameState:
     """
-
     """
 
     def __init__(self, clientID):
         self.res, self.ball = sim.simxGetObjectHandle(
             clientID, "Ball", sim.simx_opmode_blocking
         )
-        # Now try to retrieve data in a blocking fashion (i.e. a service call):
-        self.res, self.leftJoint = sim.simxGetObjectHandle(
-            clientID, "Left_joint", sim.simx_opmode_blocking
-        )
-        self.res, self.rightJoint = sim.simxGetObjectHandle(
-            clientID, "Right_joint", sim.simx_opmode_blocking
-        )
-        self.res, self.ballSensor = sim.simxGetObjectHandle(
-            clientID, "Ball_Sensor", sim.simx_opmode_blocking
-        )
-        self.res, self.proximitySensor = sim.simxGetObjectHandle(
-            clientID, "Proximity_sensor", sim.simx_opmode_blocking
-        )
-        self.res, self.goalLineSensor = sim.simxGetObjectHandle(
-            clientID, "Goal_Line_Sensor", sim.simx_opmode_blocking
-        )
-
         self.res, self.goalDetectionSensorA = sim.simxGetObjectHandle(
             clientID, "Goal_Detection_Sensor_A", sim.simx_opmode_blocking
         )
+        self.get_initial_positions()
+        self.dialog = 0
+        self.messagebox = 0
+
+        self.scoreBoard = Scoreboard(clientID)
+
+    def gameLoop(self):
+        if self.messagebox == 0:
+            sim.simxEndDialog(clientID,self.dialog,sim.simx_opmode_oneshot)
+            self.messagebox = 1
+
+        _, goalScoredAState, goalScoredAData = sim.simxReadVisionSensor(
+            clientID, self.goalDetectionSensorA, sim.simx_opmode_blocking
+        )
+        goalScoredA = goalScoredAData[0][9] < 0.3
+        if goalScoredA:
+            print(goalScoredA)
+            self.scoreBoard.scoreA()
+            self.set_initial_positions()
+
+    def get_initial_positions(self):
+        self.initial_ball_position = sim.simxGetObjectPosition(clientID, self.ball, -1, sim.simx_opmode_blocking)[1]
+
+    def set_initial_positions(self):
+        sim.simxSetObjectPosition(
+            clientID, self.ball, -1, [random.uniform(-1,1), random.uniform(-0.5, 0.5), 0.05], sim.simx_opmode_blocking
+        )
+
+class SoccerRobot:
+    """
+
+    """
+
+    def __init__(self, clientID, robotLetter):
+        # Now try to retrieve data in a blocking fashion (i.e. a service call):
+        self.res, self.leftJoint = sim.simxGetObjectHandle(
+            clientID, f"Left_joint_{robotLetter}", sim.simx_opmode_blocking
+        )
+        self.res, self.rightJoint = sim.simxGetObjectHandle(
+            clientID, f"Right_joint_{robotLetter}", sim.simx_opmode_blocking
+        )
+        self.res, self.ballSensor = sim.simxGetObjectHandle(
+            clientID, f"Ball_Sensor_{robotLetter}", sim.simx_opmode_blocking
+        )
+        self.res, self.proximitySensor = sim.simxGetObjectHandle(
+            clientID, f"Proximity_sensor_{robotLetter}", sim.simx_opmode_blocking
+        )
+        self.res, self.goalLineSensor = sim.simxGetObjectHandle(
+            clientID, f"Goal_Line_Sensor_{robotLetter}", sim.simx_opmode_blocking
+        )
+
         sim.simxSetJointTargetVelocity(
             clientID, self.leftJoint, 1, sim.simx_opmode_oneshot
         )
@@ -62,18 +153,8 @@ class SoccerRobot:
             self.proximitySensor,
             self.goalLineSensor,
         ]
-        self.get_initial_positions()
-        self.dialog = 0
-        self.messagebox = 0
 
-    def get_initial_positions(self):
-        self.initial_ball_position = sim.simxGetObjectPosition(clientID, self.ball, -1, sim.simx_opmode_blocking)[1]
-
-    def set_initial_positions(self):
-        sim.simxSetObjectPosition(
-            clientID, self.ball, -1, self.initial_ball_position, sim.simx_opmode_blocking
-        )
-        _,self.dialog,_ = sim.simxDisplayDialog(clientID, " ", "Begin!", 0, " ", None , None ,sim.simx_opmode_blocking)
+    
 
     def update_robot(self, clientID) -> bool:
         _, ballDetectionState, ballSensorData = sim.simxReadVisionSensor(
@@ -85,22 +166,11 @@ class SoccerRobot:
         _, goalLineSensorState, goalLineSensorData = sim.simxReadVisionSensor(
             clientID, self.goalLineSensor, sim.simx_opmode_blocking
         )
-        _, goalScoredAState, goalScoredAData = sim.simxReadVisionSensor(
-            clientID, self.goalDetectionSensorA, sim.simx_opmode_blocking
-        )
-        
-        if self.messagebox == 0:
-            sim.simxEndDialog(clientID,self.dialog,sim.simx_opmode_oneshot)
-            self.messagebox = 1
-        
+
+
         facingBall = ballSensorData[0][9] < 0.3
         touchingBall = proxDetectionState and detectedPoint[2] < 0.15
         facingGoalLine = goalLineSensorData[0][9] < 0.3
-        goalScoredA = goalScoredAData[0][9] < 0.3
-        if goalScoredA:
-            print(goalScoredA)
-        if goalScoredA and facingGoalLine:
-            self.set_initial_positions()
 
         if facingBall:
             if not touchingBall:
@@ -153,10 +223,14 @@ if clientID != -1:
     print("Connected to remote API server")
 
     # Now try to retrieve data in a blocking fashion (i.e. a service call):
-    bot = SoccerRobot(clientID=clientID)
+    bot = SoccerRobot(clientID=clientID, robotLetter="A")
+    bot2 = SoccerRobot(clientID=clientID, robotLetter="B")
+    gamestate = GameState(clientID=clientID)
 
     while True:
+        bot2.update_robot(clientID=clientID)
         bot.update_robot(clientID=clientID)
+        gamestate.gameLoop()
 
     # Now send some data to CoppeliaSim in a non-blocking fashion:
     sim.simxAddStatusbarMessage(clientID, "Hello CoppeliaSim!", sim.simx_opmode_oneshot)
